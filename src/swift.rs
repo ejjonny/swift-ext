6d7964ac309935eee2d4d2365274e2d8e5bc220f
Ethan John <ejjonny@gmail.com>
2024-04-26 19:01:20 -0600

    Remove experiments

--- src/swift.rs
+++ src/swift.rs
@@ -1,4 +1,3 @@
-use zed_extension_api::settings::LspSettings;
 use zed_extension_api::{self as zed, Result};
 
 struct SwiftExtension {}
@@ -13,28 +12,13 @@ impl zed::Extension for SwiftExtension {
     fn language_server_command(
         &mut self,
         _server_id: &zed::LanguageServerId,
-        worktree: &zed::Worktree,
+        _worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
         Ok(zed::Command {
-            command: "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp".into(),
-            args: vec!["--log-level".into(), "debug".into(), "--scratch-path".into(), worktree.root_path()],
+            command: "/usr/bin/sourcekit-lsp".into(),
+            args: vec!["--log-level".into(), "debug".into()],
             env: Default::default(),
         })
-    }
-
-    fn language_server_workspace_configuration(
-        &mut self,
-        _language_server_id: &zed::LanguageServerId,
-        worktree: &zed::Worktree,
-    ) -> Result<Option<serde_json::Value>> {
-        let settings = LspSettings::for_worktree("sourcekit-lsp", worktree)
-            .ok()
-            .and_then(|lsp_settings| lsp_settings.settings.clone())
-            .unwrap_or_default();
-
-        Ok(Some(serde_json::json!({
-            "sourcekit-lsp": settings
-        })))
     }
 }
 
