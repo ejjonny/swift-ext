a849ada69534d81c7b2a0470a846c18870901a5c
Ethan John <ejjonny@gmail.com>
2024-05-07 09:29:57 -0600

    Use xcrun

--- src/swift.rs
+++ src/swift.rs
@@ -13,8 +13,8 @@ impl zed::Extension for SwiftExtension {
         _worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
         Ok(zed::Command {
-            command: "/usr/bin/sourcekit-lsp".into(),
-            args: vec![],
+            command: "/usr/bin/xcrun".into(),
+            args: vec!["sourcekit-lsp".into()],
             env: Default::default(),
         })
     }
